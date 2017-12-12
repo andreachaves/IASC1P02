bf32b790260119c770d0f6b91c28484560a2f895
andreachaves <31972273+andreachaves@users.noreply.github.com>
2017-12-12 13:56:33 -0500

    Add files via upload

--- scripts1.js
+++ scripts1.js
@@ -1,8 +1,20 @@
-function enter(SIGNUP) {
-    document.getElementById("output").innerHTML = "You Are Now Signed Up!";
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('projects');
+var modalImg = document.getElementById("jcole.jpg");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
 }
-function search() {
-    document.getElementById("output1").innerHTML = "sorry couldn't find it";
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
